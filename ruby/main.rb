1cb2c2b3b7059eb2edbcb39f77cd293ac64ac496
Opus1no2 <tillotson.travis@gmail.com>
2024-08-10 20:37:19 -0500

    cleanup ruby

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,13 +1,12 @@
 require 'tk'
 
-
-def init()
-  root = TkRoot.new { title "Conways Game of Life" }
+def init
+  root = TkRoot.new { title 'Conways Game of Life' }
 
   canvas = TkCanvas.new(root) do
     width  500
     height 500
-    background "black"
+    background 'black'
     pack
   end
   grid = Array.new(50) { Array.new(50) { 0 } }
@@ -30,8 +29,8 @@ def seed_grid(grid)
   grid[4][1] = 1
 end
 
-def fill_cells(grid, canvas, x, y, color="white")
-  TkcRectangle.new(canvas, x*10, y*10, x*10+10, y*10+10) do
+def fill_cells(_grid, canvas, x, y, color = 'white')
+  TkcRectangle.new(canvas, x * 10, y * 10, x * 10 + 10, y * 10 + 10) do
     fill color
   end
   canvas.update
@@ -41,28 +40,28 @@ def draw_grid(grid, canvas)
   grid.each_with_index do |row, i|
     row.each_with_index do |cell, j|
       fill_cells(grid, canvas, i, j) if cell == 1
-      fill_cells(grid, canvas, i, j, "black") if cell == 0
+      fill_cells(grid, canvas, i, j, 'black') if cell.zero?
     end
   end
 
   draw_grid(grid, canvas)
 end
 
-
 def neighbor_count(grid, x, y)
   neighbors = 0
-  (x-1..x+1).each do |i|
-    (y-1..y+1).each do |j|
-      next if i < 0 || j < 0 || i >= grid.length || j >= grid[0].length
+  (x - 1..x + 1).each do |i|
+    (y - 1..y + 1).each do |j|
+      next if i.negative? || j.negative? || i >= grid.length || j >= grid[0].length
       next if i == x && j == y
+
       neighbors += grid[i][j]
     end
   end
   neighbors
 end
 
-def main()
-  grid, canvas = init()
+def main
+  grid, canvas = init
   seed_grid(grid)
 
   loop do
@@ -72,13 +71,11 @@ def main()
         if cell == 1
           if neighbors < 2 || neighbors > 3
             grid[i][j] = 0
-            fill_cells(grid, canvas, i, j, "black")
+            fill_cells(grid, canvas, i, j, 'black')
           end
-        else
-          if neighbors == 3
-            grid[i][j] = 1
-            fill_cells(grid, canvas, i, j)
-          end
+        elsif neighbors == 3
+          grid[i][j] = 1
+          fill_cells(grid, canvas, i, j)
         end
       end
     end
@@ -88,5 +85,4 @@ def main()
   Tk.mainloop
 end
 
-main()
-
+main
